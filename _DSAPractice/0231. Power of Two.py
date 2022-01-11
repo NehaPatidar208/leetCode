4ef1e399d6d50e4b75959e5e3eb1fdf2d25c4ae0
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-01-11 22:05:09 +0530

    Create 0231. Power of Two.py

---
+++ _DSAPractice/0231. Power of Two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<1:
+            return False
+        if n==1:return True
+        while n>1:
+            if n%2 != 0 :
+                return False
+            n=n//2
+        return True
