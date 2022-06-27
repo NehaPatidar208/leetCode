9573c4827a5a1c9f0f7f272ae5f9ff937e571cbf
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-06-28 00:44:10 +0530

    Update 0496. Next Greater Element I.java

--- _DSAPractice/0496. Next Greater Element I.java
+++ _DSAPractice/0496. Next Greater Element I.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> nextGrt = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for(Integer i:nums2){
+            while(!st.isEmpty() && st.peek() < i){
+                nextGrt.put(st.pop(),i);
+            }
+            st.push(i);
+        }
+        int result[]=new int[nums1.length];
+        for(int i =0;i<nums1.length;i++){
+            result[i] = nextGrt.getOrDefault(nums1[i],-1);
+        }
+        return result;
+    }
+}
