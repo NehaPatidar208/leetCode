9ef55f03be019a91472c7fd8c87081a035dbbdad
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-06-30 02:07:22 +0530

    sorting(comparator sorting)

--- _DSAPractice/0406. Queue Reconstruction by Height.java
+++ _DSAPractice/0406. Queue Reconstruction by Height.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
         List<int[]> result = new ArrayList<>();
+        //if values are same sort array in increasing order of 2nd index of subarray, other wise sort array in decreasing order
         Arrays.sort(people, (a,b)-> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         for(int[] a:people){
             result.add(a[1],a);
