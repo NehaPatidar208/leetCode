9347686240d0bd15b6e0e14f8c23aeb3f02811c9
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-07-21 22:51:58 +0530

    Linear complexity

---
+++ _DSAPractice/0001. Two Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int []result=new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+                result[1]=i;
+                result[0]=map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
