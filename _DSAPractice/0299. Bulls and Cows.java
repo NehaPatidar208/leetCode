d8a5722b7b652cc1ab0764647f4201488428951b
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-07-22 23:22:30 +0530

    Update 0299. Bulls and Cows.java

--- _DSAPractice/0299. Bulls and Cows.java
+++ _DSAPractice/0299. Bulls and Cows.java
@@ -2,18 +2,13 @@ class Solution {
     public String getHint(String secret, String guess) {
         int []s = new int[10];
         int []g = new int[10];
-        for(int i=0;i<secret.length();i++){
-            s[secret.charAt(i)-'0']++;
-            g[guess.charAt(i)-'0']++;
-        }
         int cow=0,bull=0;
-        for(int i=0;i<guess.length();i++){
-            char sChar = secret.charAt(i),gChar =guess.charAt(i);
-            if(sChar==gChar){
-                s[sChar-'0']--;
-                g[gChar-'0']--;
-                cow++;
-            }
+        for(int i=0;i<secret.length();i++){
+            if(secret.charAt(i) == guess.charAt(i)) cow++;
+            else{
+                s[secret.charAt(i)-'0']++;
+            g[guess.charAt(i)-'0']++;
+            }         
         }
         for(int i=0;i<10;i++){
             bull+=Math.min(s[i],g[i]);
