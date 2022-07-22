97695e86e98268c71052acf98ea805c0f4629502
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-07-23 01:34:24 +0530

    Create 0048. Rotate Image.java

---
+++ _DSAPractice/0048. Rotate Image.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int t=matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = t;
+            }
+        }
+    }
+}
