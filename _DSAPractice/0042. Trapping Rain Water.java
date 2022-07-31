3084f05e8d2031b81735d706f30bcbe0cbdb8144
NEHA PATIDAR <52350560+NehaPatidar208@users.noreply.github.com>
2022-08-01 01:50:18 +0530

    Update 0042. Trapping Rain Water.java

--- _DSAPractice/0042. Trapping Rain Water.java
+++ _DSAPractice/0042. Trapping Rain Water.java
@@ -3,17 +3,17 @@ class Solution {
         if(height.length<3)return 0;
         int result = 0, n = height.length;
         int leftMax[] = new int[n];
-        int rightMax[] = new int[n];
         leftMax[0]=height[0];
-        rightMax[n-1] = height[n-1];
+        int rightMax = height[n-1];
         for(int i=1;i<n;i++){
             leftMax[i]=Math.max(height[i],leftMax[i-1]);
         }
         for(int i=n-2;i>=0;i--){
-            rightMax[i]=Math.max(height[i],rightMax[i+1]);
+            rightMax=Math.max(height[i],rightMax);
+            leftMax[i] = Math.min(leftMax[i],rightMax);
         }
         for(int i=1;i<n-1;i++){
-            result+= Math.min(leftMax[i],rightMax[i])- height[i];
+            result+= leftMax[i]- height[i];
         }
         return result;
     }
